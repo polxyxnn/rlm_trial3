3bfa2a466fe472fce9cf845dd9cc65ebba84e870
polxyxnn <134563676+polxyxnn@users.noreply.github.com>
2026-03-05 02:45:24 +0000

    main67

--- streamlit_app.py
+++ streamlit_app.py
@@ -427,7 +427,7 @@ with st.sidebar:
 
             .logo-text {
                 font-weight: 600;
-                font-size: 250px;
+                font-size: 400px;
                 color: white;
                 line-height: 1.1;
                 margin: 0;
